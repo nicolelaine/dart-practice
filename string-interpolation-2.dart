d3e3b498ab3ec2dd3be827ed97dd4ea34b8cf55d
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-10-28 20:15:42 +0200

    Added string-interpolation-2

---
+++ string-interpolation-2.dart
@@ -0,0 +1,15 @@
+// String
+
+main () {
+    var s1 = '''
+    you can create
+    multi-line strings like this one.
+    ''';
+    
+    var s2 = """This is also also 
+    a multi-line string""";
+
+    print(s1);
+
+    print (s2);
+}
