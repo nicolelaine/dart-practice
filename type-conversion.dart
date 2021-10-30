606580658481e72590caca78fc0c91164b3da882
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2021-10-30 15:09:55 +0200

    More type conversion

---
+++ type-conversion.dart
@@ -0,0 +1,13 @@
+main () {
+    // int --> String
+   String oneAsString = 1.toString ();
+   assert (oneAsString == '1');
+
+    //double --> String
+    String piAsString = 3.14159.toStringAsFixed(2);
+    assert(piAsString == '3.14');
+
+}
+
+//assert is a built in function
+//parse is a method
